13b29cc1286ce9d6d50c4bcff18eab15cafdfbb4
jhearn <jhearn@nerdery.com>
2015-02-20 16:39:23 -0600

    Episode completed ajax in progress.

--- app/controllers/watch_marker_episodes_controller.rb
+++ app/controllers/watch_marker_episodes_controller.rb
@@ -3,6 +3,9 @@ class WatchMarkerEpisodesController < ApplicationController
   def toggle_completed
     @ep = WatchMarkerEpisode.find(params[:id])
     @ep.toggle!(:completed)
-    render nothing: true
+    #render nothing: true
+    redirect_to do |format|
+      format.js
+    end
   end
 end
