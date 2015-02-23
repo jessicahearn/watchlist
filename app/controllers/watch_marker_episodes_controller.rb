dd5e8c0fa28264b27d487a8e01a8ccacc4e9c4f0
jhearn <jhearn@nerdery.com>
2015-02-23 12:07:17 -0600

    Finally got a non-ajax refresh working on the toggle_completed. Saving before clearing out the remains of the ajax attempt for future reference.

--- app/controllers/watch_marker_episodes_controller.rb
+++ app/controllers/watch_marker_episodes_controller.rb
@@ -2,10 +2,14 @@ class WatchMarkerEpisodesController < ApplicationController
   
   def toggle_completed
     @ep = WatchMarkerEpisode.find(params[:id])
+    @part = WatchMarkerPart.find(@ep.watch_marker_part_id)
+    @marker = WatchMarker.find(@part.watch_marker_id)
+    @user = User.find(@marker.user_id)
     @ep.toggle!(:completed)
+    redirect_to @user
     #render nothing: true
-    redirect_to do |format|
-      format.js
-    end
+    #redirect_to do |format|
+     # format.js
+    #end
   end
 end
