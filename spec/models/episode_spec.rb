7a5a0b251504c878810a4699029d17192f094ef1
Jessica Hearn <jhearn@nerdery.com>
2015-05-15 17:43:58 -0500

    Figured out my syntax mistake with rspec -- initial tests working.

--- spec/models/episode_spec.rb
+++ spec/models/episode_spec.rb
@@ -1,15 +1,15 @@
 require "rails_helper"
 
 RSpec.describe Episode do
-  @program = Program.new(title: 'Yokai Ningen Bem', description: 'Three monsters wander the earth helping humans while searching for their own humanity', format: 'Renzoku')
-  @program.save
-  @part = @program.parts.create(title: 'Series 1')
-  @part.save
-  @episode = @part.episodes.create(title: 'Episode 1', description: 'New folks in town', time: 3056)
+  it "should be valid" do
   
-  it "is valid" do
+    @program = Program.new(title: 'Yokai Ningen Bem', description: 'Three monsters wander the earth helping humans while searching for their own humanity', format: 'Renzoku')
+    @program.save
+    @part = @program.parts.create(title: 'Series 1')
+    @part.save
+    @episode = @part.episodes.create(title: 'Episode 1', description: 'New folks in town', time: 3056)
+  
+  
     expect(@episode).to be_valid
   end
 end
-
-# NOT WORKING. Nil class error.
--- spec/models/program_spec.rb
+++ spec/models/program_spec.rb
@@ -1,8 +1,10 @@
 require "rails_helper"
 
 RSpec.describe Program do
-  @program = Program.new(title: 'Yokai Ningen Bem', description: 'Three monsters wander the earth helping humans while searching for their own humanity', format: 'Renzoku')
   it "should be valid" do
+    
+    @program = Program.new(title: 'Yokai Ningen Bem', description: 'Three monsters wander the earth helping humans while searching for their own humanity', format: 'Renzoku')
+  
     expect(@program).to be_valid
   end
 end
